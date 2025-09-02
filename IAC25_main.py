6e6aa4808bb4442a60aa98a22188b8b9b28fd2a9
Conall98 <conalldepaor@gmail.com>
2025-09-02 18:07:38 +0200

    x

--- IAC25_main.py
+++ IAC25_main.py
@@ -25,8 +25,8 @@ lander = IAC_sizing_algorithm(mp, dv, Isp, model)
 #%% Monte-Carlo Class 1 uncertainties
 n = 1000
 mp_samples = np.random.normal(2000, 0, n)      # Monte Carlo for mp
-dv_samples = np.random.normal(2500, 250, n)   # Monte Carlo for dv
-isp_samples = np.random.normal(350, 5, n)    # Monte Carlo for Isp
+dv_samples = np.random.normal(2500, 0, n)   # Monte Carlo for dv
+isp_samples = np.random.normal(350, 0, n)    # Monte Carlo for Isp
 
 lander_mc = monte_carlo_IAC(n, mp_samples, dv_samples, isp_samples, IAC_sizing_algorithm, model)
 
@@ -36,14 +36,14 @@ plot_lander_scurves_with_percentiles(lander_mc)
 #%% Monte-Carlo Class 2 uncertainties
 model = mixed_estimation
 
-n = 10000
-mp_samples = np.random.normal(2000, 0, n)      # Monte Carlo for mp
-dv_samples = np.random.normal(2500, 0, n)   # Monte Carlo for dv
-isp_samples = np.random.normal(350, 0, n)    # Monte Carlo for Isp
+n = 1000
+mp_samples = np.random.normal(2000, 10, n)      # Monte Carlo for mp
+dv_samples = np.random.normal(2500, 50, n)   # Monte Carlo for dv
+isp_samples = np.random.normal(350, 5, n)    # Monte Carlo for Isp
 
 lander_mc = monte_carlo_IAC(n, mp_samples, dv_samples, isp_samples, IAC_sizing_algorithm_star, model)
 
-plot_lander_histograms(lander_mc, bins=80)
+plot_lander_histograms(lander_mc, bins=40)
 plot_lander_scurves_with_percentiles(lander_mc)
 
 
